b599cdb758ab09d18ec2ad9865b537efc0cb7a69
diyanarmln <diyana.rmln@gmail.com>
2022-06-28 22:01:41 +0800

    added material UI theme color palette

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 
 import React from "react";
 import axios from "axios";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+
 
 // make sure that axios always sends the cookies to the backend server
 axios.defaults.withCredentials = true;
@@ -16,13 +20,48 @@ const testReq = () => {
   });
 };
 
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#5948D3',
+      contrastText: '#efefef',
+    },
+    secondary: {
+      main: '#CF65F2',
+    },
+    background: {
+      default: '#262431',
+      paper: '#262431',
+    },
+    text: {
+      primary: '#efefef',
+      disabled: '#B4B4B4',
+      secondary: '#EFEFEF',
+      hint: '#EFEFEF',
+    },
+    info: {
+      main: '#CF65F2',
+    },
+    success: {
+      main: '#27A37A',
+    },
+    divider: '#CF65F2',
+    warning: {
+      main: '#F0B57D',
+    },
+  },
+});
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <button type="button" onClick={testReq}>
+      <Button type="button" variant="contained" color="primary" onClick={testReq}>
         Test!
-      </button>
+      </Button>
     </div>
+    </ThemeProvider>
   );
 }
 
