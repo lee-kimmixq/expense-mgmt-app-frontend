f44cf4d3328bc23f079567cfd738440ca123bacf
diyanarmln <diyana.rmln@gmail.com>
2022-06-28 22:17:12 +0800

    created first component - main app title

---
+++ src/components/UI/atoms/AppTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+export default function AppTitle() {
+  return (
+    <div>
+      <h1 style={{textDecoration:'line-through'}}>make sense</h1>
+      <h1>make cents</h1>
+    </div>
+  );
+}
+
