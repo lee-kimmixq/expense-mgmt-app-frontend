dcc311f862974631d674b2d8b117211d289e8b41
diyanarmln <diyana.rmln@gmail.com>
2022-06-28 22:20:28 +0800

    atom component for standalone title

---
+++ src/components/UI/atoms/AppTitleStandalone.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import axios from "axios";
+
+export default function AppTitleStandalone () {
+  return (
+    <div>
+      <h1>make cents</h1>
+    </div>
+  );
+}
+
