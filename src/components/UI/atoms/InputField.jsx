1e135956c767b56fa44a0f6a43b1c14a1276908c
diyanarmln <diyana.rmln@gmail.com>
2022-06-29 21:48:14 +0800

    remove unused line

--- src/components/UI/atoms/InputField.jsx
+++ src/components/UI/atoms/InputField.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { TextField } from "@mui/material";
-import { styled } from '@mui/material/styles';
 // import axios from "axios";
 
 export default function InputField ({fieldName, fieldType, isRequired, fieldLabel, fieldHelperText, handleChange}) {
