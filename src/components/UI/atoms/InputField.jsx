5a1d387cf02323e4ee2fb96d0de81ef1b3ba8696
diyanarmln <diyana.rmln@gmail.com>
2022-06-29 21:47:11 +0800

    add atom component for input field

---
+++ src/components/UI/atoms/InputField.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { TextField } from "@mui/material";
+import { styled } from '@mui/material/styles';
+// import axios from "axios";
+
+export default function InputField ({fieldName, fieldType, isRequired, fieldLabel, fieldHelperText, handleChange}) {
+
+  return (
+    <div>
+      <TextField 
+        id={fieldName}
+        label={fieldLabel}
+        type={fieldType}
+        helperText={fieldHelperText}
+        onChange={handleChange}
+        required={isRequired}
+        />
+    </div>
+  );
+}
+
