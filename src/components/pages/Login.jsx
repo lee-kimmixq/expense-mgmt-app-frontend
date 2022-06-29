430663b99bbec8a381aeb7cffc963d58c6c68c01
diyanarmln <diyana.rmln@gmail.com>
2022-06-29 23:20:39 +0800

    added login page component

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import LoginForm from "../UI/molecules/LoginForm.jsx";
+import AppTitle from "../UI/atoms/AppTitle.jsx"
+import Box from "@mui/material/Box"
+// import axios from "axios";
+
+export default function Login () {
+  return (
+    <Box
+    sx={{
+        display: 'inline-flex',
+        flexDirection: 'column',
+        rowGap: '10px',
+        width: '60%',
+      }}
+    >
+    <AppTitle />
+    <LoginForm />
+    </Box>
+      
+  );
+}
+
