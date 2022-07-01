d2a263485022bf388b8267c3633b14b4369d0850
diyanarmln <diyana.rmln@gmail.com>
2022-07-01 22:14:30 +0800

    added atom component for tab

---
+++ src/components/UI/atoms/NavTab.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Tab } from "@mui/material";
+// import axios from "axios";
+
+export default function NavTab ({tabName, tabValue}) {
+
+  return (
+      <Tab value={tabName} label={tabValue} />
+  );
+}
