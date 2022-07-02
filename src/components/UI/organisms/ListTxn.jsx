eb6b2eaf2582c592a1133a9b3e39f2a2ee4f65a4
diyanarmln <diyana.rmln@gmail.com>
2022-07-02 12:17:50 +0800

    added organism component f or listing transactions

---
+++ src/components/UI/organisms/ListTxn.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import axios from "axios";
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import CategoryAvatar from "../molecules/CategoryAvatar.jsx";
+import ListTxnText from "../atoms/ListTxnText.jsx";
+
+export default function ListTxn ({textValue}) {
+
+  return (
+    <List dense={true}>
+        <ListItem>
+          <Box sx={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+            <ListItemAvatar>
+            <CategoryAvatar categoryName={'fnb'}/>
+          </ListItemAvatar>
+          <ListTxnText textValue={'test'}/>
+          </Box>
+          <Box>
+            <ListTxnText ege="end" textValue={'$34'}/>
+          </Box>
+        </ListItem>,
+    </List>  
+  );
+}
