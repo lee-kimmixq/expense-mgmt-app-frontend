93b2c26bf2d9d785fca5253c62e00d97c500ef04
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-02 14:43:14 +0800

    added fieldValue prop in InputField component

--- src/components/UI/atoms/InputField.jsx
+++ src/components/UI/atoms/InputField.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { TextField } from "@mui/material";
 // import axios from "axios";
 
-export default function InputField ({fieldName, fieldType, isRequired, fieldLabel, fieldHelperText, handleChange}) {
+export default function InputField ({fieldName, fieldType, fieldValue, isRequired, fieldLabel, fieldHelperText, handleChange}) {
 
   return (
       <TextField 
         id={fieldName}
         label={fieldLabel}
         type={fieldType}
+        value={fieldValue}
         helperText={fieldHelperText}
         onChange={handleChange}
         required={isRequired}
