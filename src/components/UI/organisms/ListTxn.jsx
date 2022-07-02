cd4cad3a24837e672fd928b53e525b08121b8549
diyanarmln <diyana.rmln@gmail.com>
2022-07-02 13:07:40 +0800

    Updated list txn molecule component to list an array of objects

--- src/components/UI/organisms/ListTxn.jsx
+++ src/components/UI/organisms/ListTxn.jsx
@@ -7,24 +7,43 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import CategoryAvatar from "../molecules/CategoryAvatar.jsx";
 import ListTxnText from "../atoms/ListTxnText.jsx";
 
-export default function ListTxn ({textValue}) {
+export default function ListTxn () {
 
-  return (
-    <List dense={true}>
-        <ListItem
+  const testTxns = [
+    {
+      id: 1,
+      catName: 'fnb',
+      txnName: 'KFC',
+      amount: '$34',
+    },
+    {
+      id: 2,
+      catName: 'transport',
+      txnName: 'Grab',
+      amount: '$11',
+    },
+  ]
+
+  const txnList = testTxns.length === 0 ? <p>No Transactions Available</p> : testTxns.map((txn) => (
+    <ListItem
+          disableGutters
           secondaryAction={
-            <ListTxnText ege="end" textValue={'$34'}/>
+            <ListTxnText ege="end" textValue={txn.amount}/>
           }
+          key={`txn${txn.id}`}
         >
           <Box sx={{display:'flex', flexDirection:'row', alignItems:'center'}}>
             <ListItemAvatar>
-            <CategoryAvatar categoryName={'fnb'}/>
-          </ListItemAvatar>
-          <ListTxnText textValue={'test'}/>
+              <CategoryAvatar categoryName={txn.catName}/>
+            </ListItemAvatar>
+            <ListTxnText textValue={txn.txnName}/>
           </Box>
-          <Box>
-          </Box>
-        </ListItem>,
+        </ListItem>
+  ));
+
+  return (
+    <List dense>
+        {txnList}
     </List>  
   );
 };
