3780227104363ea58bc3be3255044907528bf82d
diyanarmln <diyana.rmln@gmail.com>
2022-07-03 03:18:48 +0800

    added prop for default value and removed asterisk for required fields for ux reasons

--- src/components/UI/atoms/InputField.jsx
+++ src/components/UI/atoms/InputField.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { TextField } from "@mui/material";
 // import axios from "axios";
 
-export default function InputField ({fieldName, fieldType, isRequired, fieldLabel, fieldHelperText, handleChange}) {
+export default function InputField ({fieldName, fieldType, isRequired, fieldLabel, fieldHelperText, handleChange, fieldValue}) {
 
   return (
       <TextField 
+        InputLabelProps={{ 
+          required: false,
+         }}
         id={fieldName}
         label={fieldLabel}
         type={fieldType}
         helperText={fieldHelperText}
         onChange={handleChange}
         required={isRequired}
+        defaultValue={fieldValue}
         size="small"
         />
   );
