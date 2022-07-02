70a111f131246bebe7f2893cc65e3018757a1801
diyanarmln <diyana.rmln@gmail.com>
2022-07-03 03:21:26 +0800

    added atomic component for custom input field of am ount on add txn page

---
+++ src/components/UI/atoms/TxnAmtField.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { TextField } from "@mui/material";
+// import axios from "axios";
+
+export default function TxnAmtField ({fieldName, fieldType, isRequired, fieldLabel, fieldHelperText, handleChange, fieldValue}) {
+
+  return (
+      <TextField 
+        InputLabelProps={{ 
+          required: false,
+         }}
+         InputProps={{
+          style: {
+            fontWeight: 'bolder',
+            fontSize: 'xx-large',
+          }
+         }}
+        id={fieldName}
+        label={fieldLabel}
+        type={fieldType}
+        helperText={fieldHelperText}
+        onChange={handleChange}
+        required={isRequired}
+        defaultValue={fieldValue}
+        size="small"
+        />
+  );
+}
+
