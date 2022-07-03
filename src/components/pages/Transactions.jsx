aa8f6825d7481c10a0060a3400679b81d17aab6c
diyanarmln <diyana.rmln@gmail.com>
2022-07-04 00:44:36 +0800

    created and added txn date component

--- src/components/pages/Transactions.jsx
+++ src/components/pages/Transactions.jsx
@@ -3,10 +3,44 @@ import Box from "@mui/material/Box"
 import PageHeader from "../UI/atoms/PageHeader.jsx"; 
 import NavBar from "../UI/organisms/NavBar.jsx"
 import TxnsNav from "../UI/molecules/TxnsNav.jsx";
+import ListTxnsByDate from "../UI/organisms/ListTxnByDate.jsx";
 
 // import axios from "axios";
 
 export default function Transactions () {
+
+  const testTxns = [
+    {
+      id: 1,
+      catName: 'fnb',
+      txnName: 'KFC',
+      amount: '$34',
+    },
+    {
+      id: 2,
+      catName: 'transport',
+      txnName: 'Grab',
+      amount: '$11',
+    },
+    {
+      id: 3,
+      catName: 'fnb',
+      txnName: 'KFC',
+      amount: '$34',
+    },
+    {
+      id: 4,
+      catName: 'transport',
+      txnName: 'Grab',
+      amount: '$11',
+    },
+    {
+      id: 5,
+      catName: 'fnb',
+      txnName: 'KFC',
+      amount: '$34',
+    },
+  ]
 
   return (
     <Box
@@ -20,6 +54,7 @@ export default function Transactions () {
       
       <PageHeader pageTitle={`Transactions`} />
       <TxnsNav />
+      <ListTxnsByDate txns={testTxns} />
       <NavBar />
     </Box>
   );
