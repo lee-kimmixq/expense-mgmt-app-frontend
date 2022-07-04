5665d0d8ecd6ffe3754ab920d077b48ec4d28b13
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-05 00:25:21 +0800

    added state and alert to check for matching passwords

--- src/components/UI/molecules/SignupForm.jsx
+++ src/components/UI/molecules/SignupForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import InputField from "../atoms/InputField";
 import PrimaryBtn from "../atoms/PrimaryBtn";
+import Alert from "@mui/material/Alert";
 import Box from "@mui/material/Box"
 // import axios from "axios";
 
@@ -10,6 +11,7 @@ export default function SignupForm () {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [retypePassword, setRetypePassword] = useState("");
+  const [isPasswordMatch, setIsPasswordMatch] = useState(true);
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -25,10 +27,12 @@ export default function SignupForm () {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+    setIsPasswordMatch(e.target.value === retypePassword);
   }
 
   const handleRetypePasswordChange = (e) => {
     setRetypePassword(e.target.value);
+    setIsPasswordMatch(password === e.target.value);
   }
 
   return (
@@ -40,6 +44,7 @@ export default function SignupForm () {
         rowGap: '10px',
       }}
       >
+        {!isPasswordMatch && <Alert severity={'warning'} sx={{marginBottom: '10px', textAlign: 'left', fontSize: '0.8em'}}>Passwords do not match!</Alert>}
         <InputField fieldName={'signupUsername'} fieldType={'text'} fieldAttribute={'required'} fieldLabel={'username'} isRequired={true} handleChange={handleUsernameChange}/>
         <InputField fieldName={'signupMobile'} fieldType={'tel'} fieldAttribute={'required'} fieldLabel={'contact number'} isRequired={true} handleChange={handleMobileChange}/>
         <InputField fieldName={'signupEmail'} fieldType={'email'} fieldAttribute={'required'} fieldLabel={'email'} isRequired={true} handleChange={handleEmailChange}/>
