4ed56c859c8ae831d9ba1b0ee65c89656c8598e0
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-05 03:54:23 +0800

    added delete functionality for txns

--- src/components/pages/TxnForm.jsx
+++ src/components/pages/TxnForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box"
 // import CenteredPageHeader from "../UI/atoms/CenteredPageHeader.jsx";
 import AddTxnForm from "../UI/molecules/AddTxnForm.jsx";
@@ -6,10 +7,19 @@ import UploadReceiptBtn from "../UI/molecules/UploadReceiptBtn.jsx";
 import GenerateIconLarge from "../UI/atoms/GenerateIconLarge.jsx";
 import NavBar from "../UI/organisms/NavBar.jsx"
 import PrimaryBtn from "../UI/atoms/PrimaryBtn.jsx";
-// import axios from "axios";
+import axios from "axios";
 
 export default function TxnForm () {
   const txnId = window.location.pathname.split('/')[2];
+  
+  let navigate = useNavigate();
+
+  const handleTxnDelete = () => {
+    axios.delete(`http://localhost:3004/transactions/${txnId}`)
+      .then(({ data }) => {
+        if(data.success) navigate("/txns", { replace: true });
+      })
+  }
 
   return (
     <Box
@@ -26,7 +36,7 @@ export default function TxnForm () {
       <UploadReceiptBtn />
       <AddTxnForm txnId={txnId}/>
       <NavBar />  
-      {txnId !== "add" && <PrimaryBtn buttonLabel={'Delete'} buttonColorPalette={'error'} />}
+      {txnId !== "add" && <PrimaryBtn buttonLabel={'Delete'} buttonColorPalette={'error'} onClickCallback={handleTxnDelete}/>}
     </Box>
       
   );
