057404cb58add66951ad4b4f5f6903382a1d922c
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-05 00:34:20 +0800

    added form submit handler and useswr call

--- src/components/UI/molecules/SignupForm.jsx
+++ src/components/UI/molecules/SignupForm.jsx
@@ -3,22 +3,35 @@ import InputField from "../atoms/InputField";
 import PrimaryBtn from "../atoms/PrimaryBtn";
 import Alert from "@mui/material/Alert";
 import Box from "@mui/material/Box"
-// import axios from "axios";
+import useSWR from "swr";
+import fetcherPost from "../../../utils/fetcherPost.mjs";
 
 export default function SignupForm () {
   const [username, setUsername] = useState("");
-  const [mobile, setMobile] = useState("");
+  const [contact, setContact] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [retypePassword, setRetypePassword] = useState("");
   const [isPasswordMatch, setIsPasswordMatch] = useState(true);
+  const [shouldFetch, setShouldFetch] = useState(false); 
+
+  const onSuccess = (data) => {
+    setShouldFetch(false);
+    if (data.signup) console.log('yay');
+  }
+
+  const onError = (error) => {
+    setShouldFetch(false);
+  }
+
+  useSWR(isPasswordMatch && shouldFetch ? [`http://localhost:3004/signup`, { username, contact, email, password }] : null, fetcherPost, {onSuccess, onError});
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   }
 
-  const handleMobileChange = (e) => {
-    setMobile(e.target.value);
+  const handleContactChange = (e) => {
+    setContact(e.target.value);
   }
 
   const handleEmailChange = (e) => {
@@ -35,6 +48,10 @@ export default function SignupForm () {
     setIsPasswordMatch(password === e.target.value);
   }
 
+  const handleSignupFormSubmit = () => {
+    setShouldFetch(true);
+  }
+
   return (
     <Box
       component="form"
@@ -46,11 +63,11 @@ export default function SignupForm () {
       >
         {!isPasswordMatch && <Alert severity={'warning'} sx={{marginBottom: '10px', textAlign: 'left', fontSize: '0.8em'}}>Passwords do not match!</Alert>}
         <InputField fieldName={'signupUsername'} fieldType={'text'} fieldAttribute={'required'} fieldLabel={'username'} isRequired={true} handleChange={handleUsernameChange}/>
-        <InputField fieldName={'signupMobile'} fieldType={'tel'} fieldAttribute={'required'} fieldLabel={'contact number'} isRequired={true} handleChange={handleMobileChange}/>
+        <InputField fieldName={'signupMobile'} fieldType={'tel'} fieldAttribute={'required'} fieldLabel={'contact number'} isRequired={true} handleChange={handleContactChange}/>
         <InputField fieldName={'signupEmail'} fieldType={'email'} fieldAttribute={'required'} fieldLabel={'email'} isRequired={true} handleChange={handleEmailChange}/>
         <InputField fieldName={'signupPwd'} fieldType={'password'} fieldAttribute={'required'} fieldLabel={'password'} isRequired={true} handleChange={handlePasswordChange}/>
         <InputField fieldName={'signupRetypePwd'} fieldType={'password'} fieldAttribute={'required'} fieldLabel={'re-type password'} isRequired={true} handleChange={handleRetypePasswordChange}/>
-        <PrimaryBtn marginTop={'20px'} buttonLabel={'Create Account'} />
+        <PrimaryBtn marginTop={'20px'} buttonLabel={'Create Account'} onClickCallback={handleSignupFormSubmit}/>
       </Box>
       
   );
