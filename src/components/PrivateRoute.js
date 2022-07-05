b9a9a4cbe7e6ea1ccf31062a63ec4eaf454c5edb
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-05 23:48:14 +0800

    amended return of private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,18 +1,9 @@
 import React from "react";
-import { useNavigate, Route } from "react-router-dom";
+import { Navigate, Route } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext.js";
 
-export default function PrivateRoute({ component: Component, ...rest }) {
+export default function PrivateRoute({ children }) {
   const { auth } = useAuth();
 
-  let navigate = useNavigate();
-
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        auth ? <Component {...props} /> : navigate("/login", { replace: true })
-      }
-    ></Route>
-  );
+  return auth ? children : <Navigate to="/login" />;
 }
