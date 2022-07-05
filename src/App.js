abfbb086521eab4a8464e5f93b5d11edad21c58b
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-05 23:48:00 +0800

    added private routes to App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import TxnForm from "./components/pages/TxnForm";
 import Transactions from "./components/pages/Transactions";
 import Account from "./components/pages/Account";
 import { AuthProvider } from "./contexts/AuthContext.js";
+import PrivateRoute from "./components/PrivateRoute.js";
 
 // make sure that axios always sends the cookies to the backend server
 axios.defaults.withCredentials = true;
@@ -23,13 +24,48 @@ function App() {
         <BrowserRouter>
           <Routes>
             {/* <Route path="/" element={<App />}> */}
-            <Route path="home" element={<Dashboard />} />
+            <Route
+              path="home"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
             <Route path="login" element={<Login />} />
             <Route path="signup" element={<Signup />} />
-            <Route path="txns/add" element={<TxnForm />} />
-            <Route path="txns/:id" element={<TxnForm />} />
-            <Route path="txns" element={<Transactions />} />
-            <Route path="account" element={<Account />} />
+            <Route
+              path="txns/add"
+              element={
+                <PrivateRoute>
+                  <TxnForm />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="txns/:id"
+              element={
+                <PrivateRoute>
+                  <TxnForm />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="txns"
+              element={
+                <PrivateRoute>
+                  <Transactions />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="account"
+              element={
+                <PrivateRoute>
+                  <Account />
+                </PrivateRoute>
+              }
+            />
             {/* </Route> */}
           </Routes>
         </BrowserRouter>
