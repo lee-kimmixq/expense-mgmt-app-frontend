36bba5999be77c76f48713e1e945dbac14de6cc5
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-06 00:57:23 +0800

    added render loading page if auth is null

--- src/authentication/PrivateRoute.js
+++ src/authentication/PrivateRoute.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext.js";
+import Loading from "../components/pages/Loading.jsx";
 
 export default function PrivateRoute({ children }) {
   const { auth } = useAuth();
-
+  if (auth === null) return <Loading />;
   return auth ? children : <Navigate to="/login" />;
 }
