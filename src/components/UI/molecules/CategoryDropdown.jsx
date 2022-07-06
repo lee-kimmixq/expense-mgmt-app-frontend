d0ea51c00bb7d06ee77e9a6d2d437b205f0cdfe1
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-06 22:08:00 +0800

    added fetching of category list from database to populate CategoryDropdown

--- src/components/UI/molecules/CategoryDropdown.jsx
+++ src/components/UI/molecules/CategoryDropdown.jsx
@@ -2,9 +2,31 @@ import React, { useState } from "react";
 import MenuItem from '@mui/material/MenuItem';
 import ListSubheader from '@mui/material/ListSubheader';
 import Select from '@mui/material/Select';
-// import axios from "axios";
+import useSWR from "swr";
+import fetcher from "../../../utils/fetcher.mjs";
 
 export default function CategoryDropdown ({selectValue, handleChange}) {
+  const [categories, setCategories] = useState([]);
+  const [shouldFetch, setShouldFetch] = useState(true); 
+
+  const {data, error} = useSWR(shouldFetch ? [`${process.env.REACT_APP_BACKEND_URL}/categories`] : null, fetcher.get);
+
+  if (data) {
+      setShouldFetch(false);
+      setCategories(data.categories);
+  }
+
+  const incomeCatList = categories.length === 0 ? <MenuItem value="0">
+        <em>No Categories Available</em>
+      </MenuItem> : categories.filter((category) => category.isIncome).map((category) => 
+        <MenuItem value={category.id} key={`cat${category.id}`}>{category.name}</MenuItem>
+      );
+
+  const expenseCatList = categories.length === 0 ? <MenuItem value="0">
+        <em>No Categories Available</em>
+      </MenuItem> : categories.filter((category) => !category.isIncome).map((category) => 
+        <MenuItem value={category.id} key={`cat${category.id}`}>{category.name}</MenuItem>
+      );
 
   return (
     <Select
@@ -13,17 +35,14 @@ export default function CategoryDropdown ({selectValue, handleChange}) {
       label="Category"
       variant="outlined"
       size="small"
-      select
     >
       <MenuItem value="0">
         <em>Select Category</em>
       </MenuItem>
       <ListSubheader>Income</ListSubheader>
-        <MenuItem value={1}>Option 1</MenuItem>
-        <MenuItem value={2}>Option 2</MenuItem>
+      {incomeCatList}
       <ListSubheader>Expense</ListSubheader>
-        <MenuItem value={3}>Option 3</MenuItem>
-        <MenuItem value={4}>Option 4</MenuItem>
+      {expenseCatList}
     </Select>
   );
 }
