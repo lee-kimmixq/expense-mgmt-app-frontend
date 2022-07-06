22c0b574b6ed62446957d25378ee45d429c6bae9
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-06 20:48:57 +0800

    deleted old fetcher functions (refactored)

--- src/utils/fetcherDelete.mjs
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const fetcherDelete = (url) => axios.delete(url).then((res) => res.data);
-
-export default fetcherDelete;
--- src/utils/fetcherPut.mjs
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const fetcherPut = (url, payload) =>
-  axios.put(url, payload).then((res) => res.data);
-
-export default fetcherPut;
