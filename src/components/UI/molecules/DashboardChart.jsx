8b5b9090e90c17a704bcd43238abbb2b86e800eb
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-09 02:46:38 +0800

    added label on hover

--- src/components/UI/molecules/DashboardChart.jsx
+++ src/components/UI/molecules/DashboardChart.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ResponsiveContainer, PieChart, Pie, Cell} from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, Cell, Tooltip} from 'recharts';
 import categories from "../../../utils/categories.js";
 
 export default function DashboardChart ({ data }) {
@@ -7,11 +7,12 @@ export default function DashboardChart ({ data }) {
   return (
   <ResponsiveContainer width='100%' height="20%">
     <PieChart width="80%" height="100%">
-      <Pie data={data} dataKey="total" nameKey="name" cx="50%" cy="50%" fill="#8884d8" innerRadius="50%">
+      <Pie data={data} dataKey="total" nameKey="name" cx="50%" cy="50%" fill="#8884d8">
         {data.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={categories.filter(category => category.name == entry.name)[0].color} />
         ))}
       </Pie>
+      <Tooltip formatter={(value, name) => [`$${value}`, name]} allowEscapeViewBox={{x: true, y: true}} />
     </PieChart>
   </ResponsiveContainer>
   )
