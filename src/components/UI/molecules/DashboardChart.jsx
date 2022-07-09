3df663db5aa378a077852312fd934c765c7b2342
diyanarmln <diyana.rmln@gmail.com>
2022-07-09 11:42:17 +0800

    removed pie stroke and resized pie chart

--- src/components/UI/molecules/DashboardChart.jsx
+++ src/components/UI/molecules/DashboardChart.jsx
@@ -5,14 +5,14 @@ import categories from "../../../utils/categories.js";
 export default function DashboardChart ({ data }) {
 
   return (
-  <ResponsiveContainer width='100%' height="20%">
-    <PieChart width="80%" height="100%">
-      <Pie data={data} dataKey="total" nameKey="name" cx="50%" cy="50%" fill="#8884d8">
+  <ResponsiveContainer width='100%' height="30%" >
+    <PieChart width="100%" height="100%" style={{ transform: 'scale(1)' }}>
+      <Pie data={data} dataKey="total" nameKey="name" cx="50%" cy="50%" fill="#8884d8" stroke='0' innerRadius={'40%'} outerRadius={'100%'}>
         {data.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={categories.filter(category => category.name == entry.name)[0].color} />
         ))}
       </Pie>
-      <Tooltip formatter={(value, name) => [`$${value}`, name]} allowEscapeViewBox={{y: true}} />
+      <Tooltip wrapperStyle={{backgroundColor: '#000000'}} formatter={(value, name) => [`$${value}`, name]} allowEscapeViewBox={{y: true}} />
     </PieChart>
   </ResponsiveContainer>
   )
