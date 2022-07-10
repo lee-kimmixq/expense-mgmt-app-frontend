ab931c85d691177189b8cf62974efe5a52da64cb
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-11 00:41:38 +0800

    added cases for week and day in getFirstLastDate.mjs

--- src/utils/getFirstLastDates.mjs
+++ src/utils/getFirstLastDates.mjs
@@ -5,6 +5,21 @@ const getFirstLastDates = (duration, date) => {
   let lastDate;
 
   switch (duration) {
+    case "month":
+      firstDate = new Date(date.getFullYear(), date.getMonth(), 1);
+      lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+      break;
+
+    case "week":
+      firstDate = new Date(date.setDate(date.getDate() - 7));
+      lastDate = new Date(date.setDate(date.getDate() + 7));
+      break;
+
+    case "date":
+      firstDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+      lastDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+      break;
+
     default:
       firstDate = new Date(date.getFullYear(), date.getMonth(), 1);
       lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
