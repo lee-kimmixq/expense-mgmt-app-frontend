ff1e18ac2d4ab251f04ed2140433cf8ac4486e58
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-11 00:23:38 +0800

    change == to === in ChartPie component

--- src/components/UI/atoms/ChartPie.jsx
+++ src/components/UI/atoms/ChartPie.jsx
@@ -11,7 +11,7 @@ export default function ChartPie ({ data, hasTooltip, height }) {
     <PieChart width="100%" height="100%" style={{ transform: 'scale(1)' }}>
       <Pie data={data} dataKey="total" nameKey="name" cx="50%" cy="50%" fill="#8884d8" stroke='0' innerRadius={'40%'} outerRadius={'100%'}>
         {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={categories.filter(category => category.name == entry.name)[0].color} />
+          <Cell key={`cell-${index}`} fill={categories.filter(category => category.name === entry.name)[0].color} />
         ))}
       </Pie>
       {hasTooltip && <Tooltip wrapperStyle={{backgroundColor: '#000000'}} formatter={(value, name) => [`$${value}`, name]} allowEscapeViewBox={{y: true}} />}
