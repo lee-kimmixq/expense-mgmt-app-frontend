173ccfefea95e30e5d6ff6c4eacbfaeee88e6b58
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-11 22:49:04 +0800

    amended tick values and properties

--- src/components/pages/Reports.jsx
+++ src/components/pages/Reports.jsx
@@ -16,15 +16,26 @@ export default function Reports () {
   const [month, setMonth] = useState(new Date());
   const [tabFocus, setTabFocus] = useState("day");
 
-  const { data: reportData, isLoading: isRepLoading } = useReports("month", month, tabFocus);
+  const { data: reportData, isLoading: isRepLoading } = useReports(tabFocus, month);
   const { data: expenseData, isLoading: isExpLoading } = useTxns("reports", "expenses", tabFocus, month);
   const { data: incomeData, isLoading: isIncLoading } = useTxns("reports", "income", tabFocus, month);
 
   if (isRepLoading || isExpLoading || isIncLoading) return <Loading />;
 
   // format reportData
-  const newData = reportData.map((day) => {return {...day, date: new Date(day.date).getDate()}});
+  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  let newData = {};
+  if (tabFocus === "day") {
+    newData = reportData.map((day) => {return {...day, date: new Date(day.date).getDate()}});
+  } else if (tabFocus === "week") {
+    newData = reportData.map((day) => {return {...day, date: `${new Date(day.date).getDate().toString()}/${Number(new Date(day.date).getMonth()) + 1}`}});
+  } else if (tabFocus === "month") {
+    newData = reportData.map((day) => {return {...day, date: `${monthNames[new Date(day.date).getMonth()]}`}});
+  }
+
   const maxSum = Math.max(...reportData.map((day) => day.sum));
+  console.log(reportData);
   console.log(newData);
   
   return (
@@ -48,7 +59,7 @@ export default function Reports () {
       }}>
       <ResponsiveContainer width='100%' height='100%' >
         <BarChart width="100%" height="100%" data={newData}>
-          <XAxis dataKey="date" />
+          <XAxis dataKey="date" tickSize={'0'} tickMargin={'5'} tick={{fontSize: '0.7em'}}/>
           <YAxis type="number" domain={[0, maxSum]} hide/>
           <Bar dataKey="sum" fill="#8884d8"/>
         </BarChart>
