afd919ea6cc852dfcb698d9386d175b2883411d5
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-11 22:35:08 +0800

    amended useReports to adjust group based on tab focused

--- src/utils/useReports.js
+++ src/utils/useReports.js
@@ -2,11 +2,16 @@ import useSWR from "swr";
 import fetcher from "./fetcher.mjs";
 import getFirstLastDates from "./getFirstLastDates.mjs";
 
-const useReports = (dateType, currDate) => {
-  const { firstDate, lastDate } = getFirstLastDates(dateType, currDate);
+const useReports = (tabFocus, currDate) => {
+  const tabFocusToDateType = { day: "month", week: "month", month: "year" };
+
+  const { firstDate, lastDate } = getFirstLastDates(
+    tabFocusToDateType[tabFocus],
+    currDate
+  );
 
   const { data, error, mutate } = useSWR(
-    `${process.env.REACT_APP_BACKEND_URL}/transactions/reports?txnDateMin=${firstDate}&txnDateMax=${lastDate}`,
+    `${process.env.REACT_APP_BACKEND_URL}/transactions/reports?txnDateMin=${firstDate}&txnDateMax=${lastDate}&group=${tabFocus}`,
     fetcher.get
   );
 
