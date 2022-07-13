3157ee55c09883afb837856011e92d38892bacf8
diyanarmln <diyana.rmln@gmail.com>
2022-07-14 00:57:18 +0800

    added budget card component and dummy data to dashboard page

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import ExpIncNav from "../UI/molecules/ExpIncNav.jsx";
 import ListTxn from "../UI/organisms/ListTxn.jsx";
-import Box from "@mui/material/Box"
 import PageHeader from "../UI/atoms/PageHeader.jsx"; 
 import TotalValuePrimary from "../UI/atoms/TotalValuePrimary.jsx"
 import LinkTxt from "../UI/atoms/LinkTxt.jsx";
@@ -9,6 +8,9 @@ import NavBar from "../UI/organisms/NavBar.jsx"
 import ChartPie from "../UI/atoms/ChartPie.jsx"
 import useTxns from "../../utils/useTxns.js";
 import Loading from "../pages/Loading.jsx"
+import BudgetCards from "../UI/molecules/BudgetCards.jsx";
+import Box from "@mui/material/Box"
+
 
 export default function Dashboard () {
   const [tabFocus, setTabFocus] = useState("expenses");
@@ -16,6 +18,52 @@ export default function Dashboard () {
   const { data, isLoading } = useTxns("dashboard", tabFocus, "month");
 
   if (isLoading) return <Loading />;
+
+  const dummyBudgets = 
+  [
+      {
+          "id": 1,
+          "userId": 1,
+          "categoryId": 1,
+          "categories": [{
+            "name": "Food & Drinks",
+            "color": "#5948D3",
+            "icon": "restaurant"
+          }],
+          "amount": "500.00",
+          "showInDashboard": true,
+          "createdAt": "2022-07-13T11:52:45.818Z",
+          "updatedAt": "2022-07-13T11:52:45.818Z"
+      },
+      {
+          "id": 2,
+          "userId": 1,
+          "categoryId": 4,
+          "categories": [{
+            "name": "Shopping",
+            "color": "#7e57c2",
+            "icon": "shopping_bag",
+          }],
+          "amount": "750.00",
+          "showInDashboard": true,
+          "createdAt": "2022-07-13T11:52:45.818Z",
+          "updatedAt": "2022-07-13T11:52:45.818Z"
+      },
+      {
+          "id": 3,
+          "userId": 1,
+          "categoryId": 2,
+            "categories": [{
+            "name": "Taxi",
+            "color": "#CF65F2",
+            "icon": "hail",
+          }],
+          "amount": "300.00",
+          "showInDashboard": false,
+          "createdAt": "2022-07-13T11:52:45.818Z",
+          "updatedAt": "2022-07-13T11:52:45.818Z"
+      }
+  ]
 
   return (
     <Box
@@ -43,10 +91,19 @@ export default function Dashboard () {
         <PageHeader pageTitle={`Budgeting`} />
         <LinkTxt linkText={'More'} linkURL={'/budgets'} />
       </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between'
+        }}
+      >
+        <BudgetCards pinnedBudgets={dummyBudgets} /> 
+      </Box>
       <br />
       <br />
       <br />
-
+      <br />
       <NavBar />
     </Box>
   );
