0e5d3cf2a529545f0e25084efab0a3c760269d17
diyanarmln <diyana.rmln@gmail.com>
2022-07-15 04:33:24 +0800

    added all required components together on account page

--- src/components/pages/Account.jsx
+++ src/components/pages/Account.jsx
@@ -2,11 +2,14 @@ import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import PageHeader from "../UI/atoms/PageHeader.jsx"; 
 import Box from "@mui/material/Box"
-import PrimaryBtn from "../UI/atoms/PrimaryBtn.jsx";
 import NavBar from "../UI/organisms/NavBar.jsx";
 import useSWR from "swr";
 import fetcher from "../../utils/fetcher.mjs";
 import { useAuth } from "../../authentication/AuthContext.js"
+import Divider from "@mui/material/Divider";
+import AccountProfileHeader from "../UI/atoms/AccountProfileHeader.jsx";
+import AccountNavTabs from "../UI/atoms/AccountNavTabs.jsx";
+
 
 export default function Account () {
   const [shouldFetch, setShouldFetch] = useState(false);
@@ -31,26 +34,52 @@ export default function Account () {
 
   return (
     <Box
-    sx={{
-      display: 'inline-flex',
-        flexDirection: 'column',
-        rowGap: '10px',
-        width: '80%',
-        marginTop: '10vmin'
-      }}
-    >
-    <Box sx={{
+      sx={{
+        display: 'inline-flex',
+          flexDirection: 'column',
+          rowGap: '10px',
+          width: '80%',
+          marginTop: '10vmin'
+        }}
+      >
+      <Box sx={{
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-between'
         }}>
           <PageHeader pageTitle={`Account`} />
-          <Box>
-            <PrimaryBtn buttonLabel={'Logout'} icon={'logout'}
-            onClickCallback={handleLogOutSubmit} 
-            />
-          </Box>
-        </Box>
+      </Box>
+      <Divider />
+      <AccountProfileHeader handleLogOutSubmit={handleLogOutSubmit} username={'John Tan'} />
+      <Divider /> 
+      <br />
+      <AccountNavTabs navName={'Profile'} navIcon={'person'} />
+      <AccountNavTabs navName={'Settings'} navIcon={'settings'} />
+      <AccountNavTabs navName={'Notifications'} navIcon={'notifications'} />
+      <AccountNavTabs navName={'Privacy'} navIcon={'lock'} />
+      <AccountNavTabs navName={'Security'} navIcon={'security'} />
+      <AccountNavTabs navName={'Help Centre'} navIcon={'support'} />
+      <AccountNavTabs navName={'About'} navIcon={'info'} />
+      <br></br>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start'
+       }
+      }>
+        <p style={{
+        fontSize: '0.8em',
+        margin: 0,
+        color: '#CF65F2'
+      }}>
+        Terms & Conditions</p>
+      <p style={{
+        fontStyle: 'italic',
+        fontSize: '0.8em',
+        margin: 0
+      }}>
+        © Kimmi&Diyana 2022</p>
+      </Box>
       <NavBar />
     </Box>
       
