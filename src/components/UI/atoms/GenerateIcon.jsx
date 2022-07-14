fb0db1c4ae623c83b7bd7a9f0d35b578134aaa0c
diyanarmln <diyana.rmln@gmail.com>
2022-07-15 04:31:24 +0800

    added prop to change scale of component

--- src/components/UI/atoms/GenerateIcon.jsx
+++ src/components/UI/atoms/GenerateIcon.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Icon } from "@mui/material";
 
 
-export default function GenerateIcon ({ name, color }) {
+export default function GenerateIcon ({ name, color, scale }) {
 
   return (
-    <Icon sx={{ color: color ? color : '#fff', transform: 'scale(0.9)' }}>{name}</Icon>
+    <Icon sx={{ color: color ? color : '#fff', transform: scale ? scale : 'scale(0.9)' }}>{name}</Icon>
   )
 };
