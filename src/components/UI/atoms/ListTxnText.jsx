24705b3d1f322fc328d74e4cf723e8c8f34358fc
diyanarmln <diyana.rmln@gmail.com>
2022-07-15 04:32:15 +0800

    added additional props to customise component

--- src/components/UI/atoms/ListTxnText.jsx
+++ src/components/UI/atoms/ListTxnText.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { ListItemText } from "@mui/material";
 // import axios from "axios";
 
-export default function ListTxnText ({textValue}) {
+export default function ListTxnText ({textValue, fontWeight, margin}) {
 
   return (
-      <ListItemText primary={textValue} />
+      <ListItemText sx={{
+        fontWeight: fontWeight,
+        margin: margin
+      }}
+      primary={textValue} />
   );
 }
