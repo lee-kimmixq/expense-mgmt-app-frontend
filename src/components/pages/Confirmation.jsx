d42f6f70d5289bba16eefde4e65e3ccaa8a10480
diyanarmln <diyana.rmln@gmail.com>
2022-07-20 03:19:44 +0800

    updated styling for confirmation page

--- src/components/pages/Confirmation.jsx
+++ src/components/pages/Confirmation.jsx
@@ -2,49 +2,41 @@ import React,  { useState } from "react";
 import { Link } from "react-router-dom";
 import useSWR from "swr";
 import fetcher from "../../utils/fetcher.mjs";
+import Home from "./Home.jsx"
 
 
-const Welcome = (props) => {
+const Confirmation = (props) => {
 
   const [shouldFetch, setShouldFetch] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [message, setMessage] = useState('');
+  const [successAlert, setSuccessAlert] = useState(false);
+  const [failAlert, setFailAlert] = useState(false);
+
+  console.log(successAlert, failAlert)
 
   const confirmationCode = window.location.pathname.split('/')[2];
 
   const onFetchSuccess = (data) => {
     if (data.verified) {
       setShouldFetch(false);
-      setMessage('Verified successfully');
+      setSuccessAlert(true);
     }
-  }
+  };
 
   const onError = (error) => {
     setShouldFetch(false);
     if (error.response.status === 404) {
       setIsError(true);
-      setMessage('User not found');
+      setFailAlert(true);
     };
-  }
+  };
 
   useSWR(shouldFetch ? `${process.env.REACT_APP_BACKEND_URL}/users/confirm/${confirmationCode}` : null, fetcher.get, { onSuccess: onFetchSuccess, onError });
 
 
   return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>
-          <strong>{message}</strong>
-        </h3>
-      </header>
-      {!isError ? <Link to={"/login"}>
-        Please Login
-      </Link> : <Link to={"/signup"}>
-        Please register a new account
-      </Link>}
-      
-    </div>
+      <Home successAlert={successAlert} failAlert={failAlert}/>
   );
 };
 
-export default Welcome;
+export default Confirmation;
