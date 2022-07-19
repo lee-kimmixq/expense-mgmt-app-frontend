593de839cf21778b6d6f9078fe94332c846c763f
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-20 01:27:26 +0800

    added states for each sort/filter field

--- src/components/UI/molecules/SortFilterDialog.jsx
+++ src/components/UI/molecules/SortFilterDialog.jsx
@@ -11,24 +11,52 @@ import MinMaxInputs from '../atoms/MinMaxInputs';
 import MultipleCategorySelectDropdown from './MultipleCategorySelectDropdown';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import getFirstLastDates from '../../../utils/getFirstLastDates.mjs'
+import getDateObj from '../../../utils/getDateObj.mjs'
 
-export default function SortFilterDialog ({handleOpen, setHandleOpen, name, handleConfirm, alertDescription, yesBtnLabel, noBtnLabel} ) {
+export default function SortFilterDialog ({handleOpen, setHandleOpen, name, alertDescription, yesBtnLabel, noBtnLabel, setFilters} ) {
+  const {firstDate, lastDate} = getFirstLastDates();
+
+  const [sortValue, setSortValue] = useState('txnDate:DESC')
+  const [txnDateFilter, setTxnDateFilter] = useState({txnDateMin: firstDate, txnDateMax: lastDate})
+  const [amountFilter, setAmountFilter] = useState({amountMin: null, amountMax: null});
   const [categoryId, setCategoryId] = useState([]);
-  const [selectedOption, setSelectedOption] = useState();
-  console.log(categoryId)
 
+  const handleSave = () => {
+    setHandleOpen(false);
+    setFilters({
+      sort: sortValue,
+      ...txnDateFilter,
+      ...amountFilter,
+      categories: categoryId
+    })
+  }
 
   const handleClose = () => {
     setHandleOpen(false);
   }; 
 
-  const handleSortChange = () => {
+  const handleSortChange = (e) => {
+    setSortValue(e.target.value);
+  }
 
+  const handleTxnDateMinChange = (e) => {
+    setTxnDateFilter({...txnDateFilter, txnDateMin: getDateObj(false, e.target.value)})
+  }
+
+  const handleTxnDateMaxChange = (e) => {
+    setTxnDateFilter({...txnDateFilter, txnDateMax: getDateObj(true, e.target.value)})
+  }
+
+  const handleAmountMinChange = (e) => {
+    setAmountFilter({...amountFilter, amountMin: e.target.value})
+  }
+
+  const handleAmountMaxChange = (e) => {
+    setAmountFilter({...amountFilter, amountMax: e.target.value})
   }
 
   const handleCategoryIdChange = (_, val) => {
-    console.log(val)
-    setSelectedOption(val)
     setCategoryId([...categoryId, val[val.length - 1].id]);
   }
 
@@ -57,12 +85,12 @@ export default function SortFilterDialog ({handleOpen, setHandleOpen, name, hand
             fullWidth={true}
             id="sort-select"
             onChange={handleSortChange}
-            defaultValue={'latestTxn'}
+            value={sortValue}
           >
-            <MenuItem value={'latestTxn'}>Latest Transactions</MenuItem>
-            <MenuItem value={'oldestTxn'}>Oldest Transactions</MenuItem>
-            <MenuItem value={'highestAmt'}>Highest Transaction Amount</MenuItem>
-            <MenuItem value={'lowestAmt'}>Lowest Transaction Amount</MenuItem>
+            <MenuItem value={'txnDate:DESC'}>Latest Transactions</MenuItem>
+            <MenuItem value={'txnDate:ASC'}>Oldest Transactions</MenuItem>
+            <MenuItem value={'amount:DESC'}>Highest Transaction Amount</MenuItem>
+            <MenuItem value={'amonut:ASC'}>Lowest Transaction Amount</MenuItem>
           </Select>
         </DialogContent>
 
@@ -77,26 +105,26 @@ export default function SortFilterDialog ({handleOpen, setHandleOpen, name, hand
           <Expand 
             expandTitle={'Transaction Date'} 
             expandContents={
-              <MinMaxInputs fieldName={'txnDate'} fieldType={'date'} />
+              <MinMaxInputs fieldName={'txnDate'} fieldType={'date'} handleMinChange={handleTxnDateMinChange} handleMaxChange={handleTxnDateMaxChange} minValue={txnDateFilter.txnDateMin} maxValue={txnDateFilter.txnDateMax} />
             } 
           />
           <Expand 
             expandTitle={'Transaction Amount'} 
             expandContents={
-              <MinMaxInputs fieldName={'txnAmt'} fieldType={'number'} />
+              <MinMaxInputs fieldName={'txnAmt'} fieldType={'number'} handleMinChange={handleAmountMinChange} handleMaxChange={handleAmountMaxChange}/>
             } 
           />
           <Expand 
             expandTitle={'Category'} 
             expandContents={
-              <MultipleCategorySelectDropdown selectValue={selectedOption} handleChange={handleCategoryIdChange} />
+              <MultipleCategorySelectDropdown handleChange={handleCategoryIdChange} />
             } 
           />
 
         </DialogContent>
         <DialogActions>
-           <Button variant="outlined" sx={{color: '#efefef', borderColor: '#999999'}} onClick={handleClose}>{noBtnLabel}</Button>
-          <Button variant="contained" sx={{color: ''}} onClick={handleClose}>{yesBtnLabel}</Button>
+          <Button variant="outlined" sx={{color: '#efefef', borderColor: '#999999'}} onClick={handleClose}>{noBtnLabel}</Button>
+          <Button variant="contained" sx={{color: ''}} onClick={handleSave}>{yesBtnLabel}</Button>
         </DialogActions>
       </Dialog>
     </div>
