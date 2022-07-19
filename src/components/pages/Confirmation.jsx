59d7e32d7c37c2c3cba5c926214f73823eef9e01
diyanarmln <diyana.rmln@gmail.com>
2022-07-20 04:01:59 +0800

    updated styling for confirmation page and alerts

--- src/components/pages/Confirmation.jsx
+++ src/components/pages/Confirmation.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 import useSWR from "swr";
 import fetcher from "../../utils/fetcher.mjs";
 import Home from "./Home.jsx"
+import Loading from "../pages/Loading.jsx"
+
 
 
 const Confirmation = (props) => {
 
   const [shouldFetch, setShouldFetch] = useState(true);
-  const [isError, setIsError] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [successAlert, setSuccessAlert] = useState(false);
   const [failAlert, setFailAlert] = useState(false);
-
-  console.log(successAlert, failAlert)
 
   const confirmationCode = window.location.pathname.split('/')[2];
 
@@ -20,22 +20,24 @@ const Confirmation = (props) => {
     if (data.verified) {
       setShouldFetch(false);
       setSuccessAlert(true);
+      setLoading(false);
     }
   };
 
   const onError = (error) => {
     setShouldFetch(false);
     if (error.response.status === 404) {
-      setIsError(true);
       setFailAlert(true);
+      setLoading(false);
     };
   };
 
   useSWR(shouldFetch ? `${process.env.REACT_APP_BACKEND_URL}/users/confirm/${confirmationCode}` : null, fetcher.get, { onSuccess: onFetchSuccess, onError });
 
+  if (loading) return <Loading />;
 
   return (
-      <Home successAlert={successAlert} failAlert={failAlert}/>
+    <Home successAlert={successAlert} failAlert={failAlert}/> 
   );
 };
 
