ae3d213a975f555757da912e3f3da461b11c4efb
diyanarmln <diyana.rmln@gmail.com>
2022-07-21 03:05:56 +0800

    added animations for app title

--- src/components/UI/atoms/AppTitle.jsx
+++ src/components/UI/atoms/AppTitle.jsx
@@ -4,8 +4,8 @@ import React from "react";
 export default function AppTitle () {
   return (
     <div>
-      <h1 style={{textDecoration:'line-through', fontWeight: 'normal', margin:0}}>make sense</h1>
-      <h1 style={{marginTop:'-10px'}}>make cents</h1>
+      <h1 style={{fontWeight: 'normal', margin:0}}><span class="strikethrough">make sense</span></h1>
+      <h1 style={{marginTop:'-10px'}} className="animate__animated animate__fadeInDownBig">make cents</h1>
     </div>
   );
 }
