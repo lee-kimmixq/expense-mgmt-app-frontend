d4a39e8cde53323675ffaebeb8761b720923bc37
diyanarmln <diyana.rmln@gmail.com>
2022-07-21 02:46:18 +0800

    added props to page header component for custom styling and animated the dahsboard page header text

--- src/components/UI/atoms/PageHeader.jsx
+++ src/components/UI/atoms/PageHeader.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 // import axios from "axios";
 
-export default function PageHeader ({pageTitle}) {
+export default function PageHeader ({pageTitle, customCss}) {
+
+  const classes = `page-header animate__animated ${customCss}`
   return (
-      <h1 className="page-header">{pageTitle}</h1>
+      <h1
+       className={classes}
+       >{pageTitle}</h1>
   );
 }
 
