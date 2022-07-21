0e5d71a7564992a0fc97ef78682df4ad18f81dd8
diyanarmln <diyana.rmln@gmail.com>
2022-07-21 23:33:01 +0800

    added prop to customise color

--- src/components/UI/atoms/GenerateIconLarge.jsx
+++ src/components/UI/atoms/GenerateIconLarge.jsx
@@ -3,14 +3,14 @@ import Icon from "@mui/material/Icon";
 import Box from "@mui/material/Box"
 
 
-export default function GenerateIconLarge ({name}) {
+export default function GenerateIconLarge ({name, color}) {
   return (
     <Box>
       <Icon
       sx={{
         fontSize: 100,
         width: '100%',
-        color: '#e0e0e0',
+        color: color,
       }}
       // className="animate__animated animate__flip"
     >
