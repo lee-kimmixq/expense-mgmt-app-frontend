9a87848e3c02958140c3c7ee6b54f09ad0633aee
diyanarmln <diyana.rmln@gmail.com>
2022-07-21 22:03:56 +0800

    added frontend form validation to txn form

--- src/components/UI/organisms/TxnForm.jsx
+++ src/components/UI/organisms/TxnForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "../atoms/InputField";
 import PrimaryBtn from "../atoms/PrimaryBtn";
 import Box from "@mui/material/Box";
@@ -6,25 +6,40 @@ import TxnAmtField from "../atoms/TxnAmtField.jsx";
 import CategoryDropdown from "../molecules/CategoryDropdown.jsx";
 import UploadReceiptBtn from "../molecules/UploadReceiptBtn.jsx";
 import ViewReceiptBtn from "../molecules/ViewReceiptBtn.jsx";
-
+import FormAlert from "../atoms/FormAlert";
 
 export default function TxnForm ({ isEditForm, photo, setPhoto, amount, setAmount, txnDate, setTxnDate, title, setTitle, categoryId, setCategoryId, imageUrl, setShouldPost }) {
+
+  const [showFormValidationError, setShowFormValidationError] = useState(false);
+  const [amountInputError, setAmountInputError] = useState(false);
+  const [txnDateInputError, setTxnDateInputError] = useState(false);
+  const [titleInputError, setTitleInputError] = useState(false);
+  const [categoryIdInputError, setCategoryIdInputError] = useState(false);
+
 
   var tzOffset = (new Date()).getTimezoneOffset() * 60000;
 
   const handleAmountChange = (e) => {
+    setShowFormValidationError(false);
+    setAmountInputError(false);
     setAmount(e.target.value);
   }
 
   const handleTxnDateChange = (e) => { // only works if use datepicker
+    setShowFormValidationError(false);
+    setTxnDateInputError(false);
     setTxnDate(new Date(e.target.value));
   }
 
   const handleTitleChange = (e) => {
+    setShowFormValidationError(false);
+    setTitleInputError(false);
     setTitle(e.target.value);
   }
 
   const handleCategoryIdChange = (_, val) => {
+    setShowFormValidationError(false);
+    setCategoryIdInputError(false);
     val && setCategoryId(val.id);
   }
 
@@ -34,8 +49,12 @@ export default function TxnForm ({ isEditForm, photo, setPhoto, amount, setAmoun
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    setShouldPost(true);
-  }     
+    (amount && txnDate && title && categoryId) ? setShouldPost(true) : setShowFormValidationError(true);
+    (!amount && setAmountInputError(true));
+    (!txnDate && setTxnDateInputError(true));
+    (!title && setTitleInputError(true));
+    (!categoryId && setCategoryIdInputError(true));
+  };
 
   return (
     <Box
@@ -47,10 +66,11 @@ export default function TxnForm ({ isEditForm, photo, setPhoto, amount, setAmoun
       }}
       >
         {isEditForm ? <ViewReceiptBtn photo={photo} imageUrl={imageUrl} handleFileUpload={handleFileUpload}/> : <UploadReceiptBtn photo={photo} handleFileUpload={handleFileUpload} buttonText={'Upload Receipt'}/>}
-        <TxnAmtField fieldName={'txnAmt'} fieldType={'number'} fieldAttribute={'required'} fieldLabel={amount ? amount : '0.00'} fieldValue={amount} isShrink={false} isRequired={true} handleChange={handleAmountChange}/>
-        <InputField fieldName={'txnDate'} fieldType={'date'} fieldAttribute={'required'} fieldValue={(new Date(txnDate - tzOffset)).toISOString().split('T')[0]} isRequired={true} handleChange={handleTxnDateChange}/>
-        <InputField fieldName={'txnName'} fieldType={'text'} fieldAttribute={'required'} fieldValue={title} fieldLabel={'Expense Name'} isRequired={true} handleChange={handleTitleChange}/>
-        <CategoryDropdown selectValue={categoryId} handleChange={handleCategoryIdChange}/>
+        {showFormValidationError && <FormAlert alertSeverity={'error'} alertLabel={'Please fill in all fields'} />}
+        <TxnAmtField fieldName={'txnAmt'} fieldType={'number'} fieldAttribute={'required'} fieldLabel={amount ? amount : '0.00'} fieldValue={amount} isShrink={false} isRequired={true} handleChange={handleAmountChange} inputError={amountInputError}/>
+        <InputField fieldName={'txnDate'} fieldType={'date'} fieldAttribute={'required'} fieldValue={(new Date(txnDate - tzOffset)).toISOString().split('T')[0]} isRequired={true} handleChange={handleTxnDateChange} inputError={txnDateInputError}/>
+        <InputField fieldName={'txnName'} fieldType={'text'} fieldAttribute={'required'} fieldValue={title} fieldLabel={'Expense Name'} isRequired={true} handleChange={handleTitleChange} inputError={titleInputError}/>
+        <CategoryDropdown selectValue={categoryId} handleChange={handleCategoryIdChange} inputError={categoryIdInputError}/>
         <PrimaryBtn btnType={'submit'} buttonLabel={'Save'} onClickCallback={handleFormSubmit}/>
       </Box>
       
